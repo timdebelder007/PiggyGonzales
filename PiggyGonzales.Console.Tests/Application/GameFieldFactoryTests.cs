ffdd7b5544c73a0deb5dbe81ff35369b68e2ba9e
Tim De Belder <tim.de.belder@itenium.be>
2022-07-26 12:49:18 +0200

    Creation Unit Test framework

---
+++ PiggyGonzales.Console.Tests/Application/GameFieldFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PiggyGonzales.Console.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyGonzales.Console.Application.Tests
+{
+    [TestClass()]
+    public class GameFieldFactoryTests
+    {
+        [TestMethod()]
+        public void CreateClosedFieldTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
