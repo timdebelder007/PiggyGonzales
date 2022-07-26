3958a3f9274cec5a68a0c057a035720a6acd1f69
Tim De Belder <tim.de.belder@itenium.be>
2022-07-26 12:40:32 +0200

    enum implementation and adding ESize enum

---
+++ PiggyGonzales.Console/Domain/ESize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyGonzales.Console.Domain;
+
+public enum ESize
+{
+    small = 1,
+    medium = 2,
+    large = 3,
+}
---
+++ PiggyGonzales.Console/Utility/EnumFuctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyGonzales.Console.Utility
+{
+    public class EnumFuctions<T> where T : struct, IConvertible
+    {
+        public static int Count 
+        { 
+            get
+            {
+                if (!typeof(T).IsEnum)
+                    throw new ArgumentException("T must be an enumerated type");
+
+                return Enum.GetNames(typeof(T)).Length;                
+            } 
+        }
+
+    }
+}
