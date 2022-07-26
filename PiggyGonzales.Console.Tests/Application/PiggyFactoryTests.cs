b7f8cd7ff7ceb8ffb004310fe2454684554b2571
Tim De Belder <tim.de.belder@itenium.be>
2022-07-26 13:34:48 +0200

    Add unit tests for Piggy Factory

---
+++ PiggyGonzales.Console.Tests/Application/PiggyFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PiggyGonzales.Console.Application;
+using PiggyGonzales.Console.Domain;
+using PiggyGonzales.Console.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyGonzales.Console.Application.Tests
+{
+    [TestClass()]
+    public class PiggyFactoryTests
+    {
+        [TestMethod()]
+        public void CreateMasterPiggyShouldHaveGivenBudget()
+        {
+            Piggy master = PiggyFactory.CreateMasterPiggy(10);
+
+            Assert.AreEqual(10, master.Budget);
+        }
+
+        [TestMethod()]
+        public void CreateMasterPiggyShouldInvalidateBudget()
+        {           
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => PiggyFactory.CreateMasterPiggy(11));            
+        }
+
+        [TestMethod()]
+        public void CreateEnemyPiggyShouldHaveBudgetInRange()
+        {
+            Piggy enemyPiggy = PiggyFactory.CreateEnemyPiggy();
+
+            Assert.IsTrue(PiggyFactory.availableEnemyBudgets.Contains(enemyPiggy.Budget));
+        }
+    }
+}
