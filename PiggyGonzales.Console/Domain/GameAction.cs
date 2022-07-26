ecf97cb08af3d4228e25bb88685d8ad45fda4d4c
Tim De Belder <tim.de.belder@itenium.be>
2022-07-26 13:51:07 +0200

    GameAction implementation + GameActionType

---
+++ PiggyGonzales.Console/Domain/GameAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyGonzales.Console.Domain
+{
+    public class GameAction
+    {
+        private readonly Piggy masterPiggy;
+        private readonly GameField gameField;
+        private readonly GameAction PreviousAction;       
+
+        public GameAction(GameAction previousAction, Piggy masterPiggy, GameField gameField)
+        {
+            PreviousAction = previousAction;
+            this.masterPiggy = masterPiggy;
+            this.gameField = gameField;
+        }        
+    }
+}
